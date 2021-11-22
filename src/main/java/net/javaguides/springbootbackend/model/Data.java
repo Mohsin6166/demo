88aa26d8fbd1780f157e8ad377f83d061fc4fce4
Mohsin6166 <92360936+Mohsin6166@users.noreply.github.com>
2021-11-22 01:37:06 -0800

    Update Data.java

--- src/main/java/net/javaguides/springbootbackend/model/Data.java
+++ src/main/java/net/javaguides/springbootbackend/model/Data.java
@@ -1,34 +1,70 @@
+// package net.javaguides.springbootbackend.model;
+
+// public class Data {
+
+//     private AppMenuModel appMenuModel;
+
+//     public AppMenuModel getAppMenuModel() {
+//         return appMenuModel;
+//     }
+
+//     public void setAppMenuModel(AppMenuModel appMenuModel) {
+//         this.appMenuModel = appMenuModel;
+//     }
+
+//     public DrawerDetails getDrawerDetails() {
+//         return drawerDetails;
+//     }
+
+//     public void setDrawerDetails(DrawerDetails drawerDetails) {
+//         this.drawerDetails = drawerDetails;
+//     }
+
+//     public PromotionModel getPromotionModel() {
+//         return promotionModel;
+//     }
+
+//     public void setPromotionModel(PromotionModel promotionModel) {
+//         this.promotionModel = promotionModel;
+//     }
+
+//     private DrawerDetails drawerDetails;
+//     private PromotionModel promotionModel;
+
+// }
+
+
 package net.javaguides.springbootbackend.model;
+
+import java.util.List;
 
 public class Data {
 
-    private AppMenuModel appMenuModel;
+    private List<AppMenuModel> appMenuModel;
+    private List<DrawerDetails> drawerDetails;
+    private List<PromotionModel> promotionModel;
 
-    public AppMenuModel getAppMenuModel() {
+    public List<AppMenuModel> getAppMenuModel() {
         return appMenuModel;
     }
 
-    public void setAppMenuModel(AppMenuModel appMenuModel) {
+    public void setAppMenuModel(List<AppMenuModel> appMenuModel) {
         this.appMenuModel = appMenuModel;
     }
 
-    public DrawerDetails getDrawerDetails() {
+    public List<DrawerDetails> getDrawerDetails() {
         return drawerDetails;
     }
 
-    public void setDrawerDetails(DrawerDetails drawerDetails) {
+    public void setDrawerDetails(List<DrawerDetails> drawerDetails) {
         this.drawerDetails = drawerDetails;
     }
 
-    public PromotionModel getPromotionModel() {
+    public List<PromotionModel> getPromotionModel() {
         return promotionModel;
     }
 
-    public void setPromotionModel(PromotionModel promotionModel) {
+    public void setPromotionModel(List<PromotionModel> promotionModel) {
         this.promotionModel = promotionModel;
     }
-
-    private DrawerDetails drawerDetails;
-    private PromotionModel promotionModel;
-
 }

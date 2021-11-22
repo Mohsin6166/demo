6a3b5f27c69df9d66845dae874a9007f42565ba1
Mohsin6166 <92360936+Mohsin6166@users.noreply.github.com>
2021-11-22 01:38:39 -0800

    Update homerepository.java

--- src/main/java/net/javaguides/springbootbackend/repository/homerepository.java
+++ src/main/java/net/javaguides/springbootbackend/repository/homerepository.java
@@ -1,3 +1,15 @@
+// package net.javaguides.springbootbackend.repository;
+
+// import net.javaguides.springbootbackend.model.HomeModel;
+// import org.springframework.data.mongodb.repository.MongoRepository;
+
+// import java.util.List;
+
+// public interface homerepository extends MongoRepository<HomeModel, String> {
+//      List<HomeModel> findAll();
+// }
+
+
 package net.javaguides.springbootbackend.repository;
 
 import net.javaguides.springbootbackend.model.HomeModel;
@@ -5,6 +17,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface homerepository extends MongoRepository<HomeModel, String> {
-     List<HomeModel> findAll();
+public interface homerepository extends MongoRepository<List<HomeModel>, String> {
+     List<List<HomeModel>> findAll();
 }

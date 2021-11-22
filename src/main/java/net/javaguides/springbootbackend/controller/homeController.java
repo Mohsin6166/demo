e94fe782fa9eeaa4675e2038982aa3c6fd7bab5f
Mohsin6166 <92360936+Mohsin6166@users.noreply.github.com>
2021-11-22 01:54:09 -0800

    Update homeController.java

--- src/main/java/net/javaguides/springbootbackend/controller/homeController.java
+++ src/main/java/net/javaguides/springbootbackend/controller/homeController.java
@@ -1,41 +1,3 @@
-// package net.javaguides.springbootbackend.controller;
-
-// import net.javaguides.springbootbackend.model.*;
-// import net.javaguides.springbootbackend.repository.homerepository;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.*;
-
-// import java.util.List;
-// import java.util.Optional;
-
-// @RestController
-// public class homeController {
-
-//     @Autowired
-//     private homerepository repository;
-
-
-//     @PostMapping("/addDrawerOption")
-//     public HomeModel saveDraweOption(@RequestBody HomeModel homes){
-// //        homes.setplace(place);
-// //        homes.setname(name);
-
-//         repository.save(homes);
-//         return homes;
-
-//     }
-//     @GetMapping("/allDrawerOptions")
-//     public HomeModel getNavDrawerOptions(){
-
-//         List<HomeModel> homeModelList = repository.findAll();
-//         HomeModel model = homeModelList.get(0);
-//         return model;
-
-//     }
-
-// }
-
-
 package net.javaguides.springbootbackend.controller;
 
 import net.javaguides.springbootbackend.model.*;
@@ -48,26 +10,28 @@ import java.util.Optional;
 
 @RestController
 public class homeController {
-    List<HomeModel> arrylist = null;
+
     @Autowired
     private homerepository repository;
 
 
     @PostMapping("/addDrawerOption")
-    public HomeModel saveDraweOption(@RequestBody List<HomeModel> homes) {
+    public HomeModel saveDraweOption(@RequestBody HomeModel homes){
 //        homes.setplace(place);
 //        homes.setname(name);
-        arrylist = repository.save(homes);
-        return (HomeModel) arrylist;
+
+        repository.save(homes);
+        return homes;
 
     }
-
     @GetMapping("/allDrawerOptions")
-    public List<HomeModel> getNavDrawerOptions() {
+    public HomeModel getNavDrawerOptions(){
 
-        List<List<HomeModel>> homeModelList = repository.findAll();
-        HomeModel model = (HomeModel) homeModelList.get(0);
-        return (List<HomeModel>) model;
+        List<HomeModel> homeModelList = repository.findAll();
+        HomeModel model = homeModelList.get(0);
+        return model;
 
     }
+
 }
+

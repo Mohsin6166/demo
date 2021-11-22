1defc612e7b6100f0ada94618575d2170817aa6e
Mohsin6166 <92360936+Mohsin6166@users.noreply.github.com>
2021-11-22 01:37:43 -0800

    Update HomeModel.java

--- src/main/java/net/javaguides/springbootbackend/model/HomeModel.java
+++ src/main/java/net/javaguides/springbootbackend/model/HomeModel.java
@@ -1,13 +1,52 @@
+// package net.javaguides.springbootbackend.model;
+
+
+// import org.springframework.data.mongodb.core.mapping.Document;
+
+// @Document(collection = "homemenus")
+// public class HomeModel {
+
+//     private Meta meta;
+//     private Data data;
+
+//     public Meta getMeta() {
+//         return meta;
+//     }
+
+//     public void setMeta(Meta meta) {
+//         this.meta = meta;
+//     }
+
+//     public Data getData() {
+//         return data;
+//     }
+
+//     public void setData(Data data) {
+//         this.data = data;
+//     }
+// }
+
+
+
 package net.javaguides.springbootbackend.model;
 
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Document(collection = "homemenus")
 public class HomeModel {
+    public List<Data> getData() {
+        return data;
+    }
+
+    public void setData(List<Data> data) {
+        this.data = data;
+    }
 
     private Meta meta;
-    private Data data;
+    private List<Data> data;
 
     public Meta getMeta() {
         return meta;
@@ -17,11 +56,4 @@ public class HomeModel {
         this.meta = meta;
     }
 
-    public Data getData() {
-        return data;
-    }
-
-    public void setData(Data data) {
-        this.data = data;
-    }
 }

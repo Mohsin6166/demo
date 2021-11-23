d5f842cecb91f085f4c47632d3284230a8132dfb
Mohsin6166 <92360936+Mohsin6166@users.noreply.github.com>
2021-11-22 23:44:35 -0800

    Update Data.java

--- src/main/java/net/javaguides/springbootbackend/model/Data.java
+++ src/main/java/net/javaguides/springbootbackend/model/Data.java
@@ -1,37 +1,3 @@
-// package net.javaguides.springbootbackend.model;
-
-// public class Data {
-
-//     private AppMenuModel appMenuModel;
-//     private DrawerDetails drawerDetails;
-//     private PromotionModel promotionModel;
-
-//     public AppMenuModel getAppMenuModel() {
-//         return appMenuModel;
-//     }
-
-//     public void setAppMenuModel(AppMenuModel appMenuModel) {
-//         this.appMenuModel = appMenuModel;
-//     }
-
-//     public DrawerDetails getDrawerDetails() {
-//         return drawerDetails;
-//     }
-
-//     public void setDrawerDetails(DrawerDetails drawerDetails) {
-//         this.drawerDetails = drawerDetails;
-//     }
-
-//     public PromotionModel getPromotionModel() {
-//         return promotionModel;
-//     }
-
-//     public void setPromotionModel(PromotionModel promotionModel) {
-//         this.promotionModel = promotionModel;
-//     }
-
-
-// }
 
 package net.javaguides.springbootbackend.model;
 

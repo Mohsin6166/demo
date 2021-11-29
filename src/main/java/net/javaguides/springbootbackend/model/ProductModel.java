4924998764c086f28798f72bc5f0ef8b0987d918
Mohsin6166 <92360936+Mohsin6166@users.noreply.github.com>
2021-11-29 16:15:58 +0500

    Add files via upload

---
+++ src/main/java/net/javaguides/springbootbackend/model/ProductModel.java
@@ -0,0 +1,25 @@
+package net.javaguides.springbootbackend.model;
+
+public class ProductModel {
+
+    String product_name;
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getProduct_description() {
+        return product_description;
+    }
+
+    public void setProduct_description(String product_description) {
+        this.product_description = product_description;
+    }
+
+    String product_description;
+
+}
